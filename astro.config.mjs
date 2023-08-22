2280a4bba80eda87a0a6dfc6a6497765d22acad3
Hammad Izhar <hammad_izhar@brown.edu>
2023-08-22 18:40:31 -0400

    feat: decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import NetlifyCMS from "astro-netlify-cms";
 
+const websiteItemFields = [
+    { label: "Name", name: "name", widget: "string", required: true },
+    { label: "Image", name: "image", widget: "image", required: true },
+    { label: "Description", name: "desc", widget: "text", required: false },
+];
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [
@@ -19,21 +25,30 @@ export default defineConfig({
                     {
                         name: "leather",
                         label: "Leather Swatches",
-                        folder: "/src/assets/leather",
-                        create: true,
-                        delete: true,
-                        fields: [
-                            {
-                                label: "Name",
-                                name: "name",
-                                widget: "string",
-                            },
-                            {
-                                label: "Image",
-                                name: "image",
-                                widget: "image",
-                            },
-                        ],
+                        folder: "/src/assets/leather_swatches",
+                        media_folder: "/src/assets/leather_swatches/imgs",
+                        fields: websiteItemFields,
+                    },
+                    {
+                        name: "fabric",
+                        label: "Fabric Swatches",
+                        folder: "/src/assets/fabric_swatches",
+                        media_folder: "/src/assets/fabric_swatches/imgs",
+                        fields: websiteItemFields,
+                    },
+                    {
+                        name: "bags",
+                        label: "Bags",
+                        folder: "/src/assets/bags",
+                        media_folder: "/src/assets/bags/imgs",
+                        fields: websiteItemFields,
+                    },
+                    {
+                        name: "jackets",
+                        label: "Jackets",
+                        folder: "/src/assets/jackets",
+                        media_folder: "/src/assets/jackets/imgs",
+                        fields: websiteItemFields,
                     },
                 ],
             },
---
+++ src/assets/bags/map-name-blakc-handbag-image-src-assets-bags-test_bag-jpeg.md
@@ -0,0 +1,4 @@
+---
+name: Blakc Handbag
+image: /src/assets/bags/test_bag.jpeg
+---
