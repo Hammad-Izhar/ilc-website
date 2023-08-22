866aa15e3c5f0dd0bdfe1843347aae0029cd8c11
Hammad Izhar <hammad_izhar@brown.edu>
2023-08-22 17:44:48 -0400

    try out netlify-cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,44 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
+    integrations: [
+        react(),
+        tailwind(),
+        NetlifyCMS({
+            config: {
+                backend: {
+                    name: "git-gateway",
+                    branch: "main",
+                },
+                collections: [
+                    {
+                        name: "leather",
+                        label: "Leather Swatches",
+                        folder: "/src/assets/leather",
+                        create: true,
+                        delete: true,
+                        fields: [
+                            {
+                                label: "Name",
+                                name: "name",
+                                widget: "string",
+                            },
+                            {
+                                label: "Image",
+                                name: "image",
+                                widget: "image",
+                            },
+                        ],
+                    },
+                ],
+            },
+        }),
+    ],
     experimental: {
         assets: true,
     },
